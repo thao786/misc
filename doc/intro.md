e435c8fdc0151723f081793a1abbc2d9a9a6f63f
thao <roseskindergarten@gmail.com>
2014-10-30 17:35:14 -0500

    grfed

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to misc
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/misc/core_test.clj
@@ -0,0 +1,7 @@
+(ns misc.core-test
+  (:require [clojure.test :refer :all]
+            [misc.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
