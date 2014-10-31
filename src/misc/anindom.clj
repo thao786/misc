12dfeb8dc267a8bf566ca85fe71eed87535f9075
thuy ng <chunglinho@gmail.com>
2014-10-30 21:35:16 -0400

    Fe

--- src/misc/anindom.clj
+++ src/misc/anindom.clj
@@ -20,7 +20,25 @@
 						(prn query)))))
 
 
-(data-entry "/home/thao/prolife" "profile" "|profile|dinner")
+(data-entry "/home/thao/recipes" "recipes" "|recipes|appetizer")
+(data-entry "/home/thao/recipes" "recipes" "|recipes|bake")
+(data-entry "/home/thao/recipes" "recipes" "|recipes|breakfast")
+(data-entry "/home/thao/recipes" "recipes" "|recipes|dessert")
+(data-entry "/home/thao/recipes" "recipes" "|recipes|diner")
+(data-entry "/home/thao/recipes" "recipes" "|recipes|drink")
+(data-entry "/home/thao/recipes" "recipes" "|recipes|mix")
+(data-entry "/home/thao/recipes" "recipes" "|recipes|recepis-for-holiday")
+(data-entry "/home/thao/recipes" "recipes" "|recipes|salads")
+(data-entry "/home/thao/recipes" "recipes" "|recipes|seasonal-recipes")
+(data-entry "/home/thao/recipes" "recipes" "|recipes|vegetables")
+
+
+(data-entry "/home/thao/homegarden" "homegarden" "|homegarden|decor-for-holiday")
+(data-entry "/home/thao/homegarden" "homegarden" "|homegarden|diy")
+(data-entry "/home/thao/homegarden" "homegarden" "|homegarden|garden-landscape")
+(data-entry "/home/thao/homegarden" "homegarden" "|homegarden|holidays")
+(data-entry "/home/thao/homegarden" "homegarden" "|homegarden|interior")
+
 
 
 (def connection (DriverManager/getConnection connection-str username password))
